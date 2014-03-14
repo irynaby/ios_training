2a59faae35d98e747bedebe2c3cadb2812c74b5c
Irina Potigova <irina.php@gmail.com>
2014-03-14 19:14:56 +0400

    Initial Commit

---
+++ projects/study/study/study/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  study
+//
+//  Created by Admin on 14/03/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
