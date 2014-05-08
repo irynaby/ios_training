3ceed1bbdd72e9d9134bed9b8cf9e86bf07ecdd4
Irina Potigova <irina.php@gmail.com>
2014-05-08 15:19:48 +0400

    Create Product View Controller

---
+++ projects/study/premium/premium/controller/CreateProductViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateProductViewController.h
+//  premium
+//
+//  Created by Admin on 08/05/2014.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateProductViewController : UIViewController
+
+@end
