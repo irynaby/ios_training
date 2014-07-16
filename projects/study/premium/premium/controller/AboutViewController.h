552c71e0ef42e8e482cf8efa56edb4a13c3237d9
Irina Potigova <irina.php@gmail.com>
2014-07-16 16:20:05 +0400

    Added AboutViewController and AutoLayout for it.

---
+++ projects/study/premium/premium/controller/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  premium
+//
+//  Created by Admin on 23/06/2014.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ projects/study/premium/premium/controller/CartViewController.h
@@ -0,0 +1,13 @@
+//
+//  CartViewController.h
+//  premium
+//
+//  Created by Admin on 24/06/2014.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CartViewController : UIViewController
+
+@end
